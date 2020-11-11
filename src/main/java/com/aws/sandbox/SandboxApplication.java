5a5501679e184b85b61237e57d9294b5d6041dac
Alena Stanisheuskaya <39532960+Lst11@users.noreply.github.com>
2020-11-11 12:50:16 +0300

    Release 1.0 (#3)
    * Feature/init config
    
    * Maven
    
    * Git ignore file
    
    * Checkstyle
    
    * Spring boot
    
    * Init controller
    
    * Feature/ckeckstyle supressions (#2)

---
+++ src/main/java/com/aws/sandbox/SandboxApplication.java
@@ -0,0 +1,19 @@
+package com.aws.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBootApp.
+ *
+ * @author alena.stanisheuskaya
+ */
+//CHECKSTYLE:OFF
+@SpringBootApplication
+public class SandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.aws.sandbox.SandboxApplication.class, args);
+    }
+}
+//CHECKSTYLE:ON
